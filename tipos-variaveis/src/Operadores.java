3cc9427d084e4cec778c383c18f71a3da67cf64f
Domingos-Savio-Jr <domingosjr97.ds@gmail.com>
2026-06-18 15:27:35 -0300

    commit 2

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,6 +1,6 @@
 public class Operadores {
 
-    public static void main (String´[] args) {
+    public static void main (String[] args) {
 
         int a, b;
 
@@ -11,5 +11,6 @@ public class Operadores {
     
     // Se trata de um operador ternário similar a um if else
     }
+    
     
 }
