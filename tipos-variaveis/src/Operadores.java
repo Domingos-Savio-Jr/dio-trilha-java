86aac98c1ebe6363ea4f27c8174cd954ebb76330
Domingos-Savio-Jr <domingosjr97.ds@gmail.com>
2026-06-18 15:24:24 -0300

    primeiro commit

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+
+    public static void main (String´[] args) {
+
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+    
+    // Se trata de um operador ternário similar a um if else
+    }
+    
+}
